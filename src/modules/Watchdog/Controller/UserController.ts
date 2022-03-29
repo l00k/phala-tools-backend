14ea3277b893bf987ca939768a8f5b972b07bde8
l00k <l00k+github@protonmail.com>
2022-03-29 04:21:47 +0200

    implementation batch

--- src/modules/Watchdog/Controller/UserController.ts
+++ src/modules/Watchdog/Controller/UserController.ts
@@ -1,22 +1,17 @@
-import { EntityManagerWrapper } from '#/BackendCore/Service/EntityManagerWrapper';
+import { CrudController } from '#/BackendCore/Controller/CrudController';
 import { User } from '#/Watchdog/Domain/Model/User';
 import { Annotation as API } from '@inti5/api-backend';
 import * as Router from '@inti5/express-ext';
-import { Inject } from '@inti5/object-manager';
-import { Logger } from '@inti5/utils/Logger';
+import { Assert } from 'core/validator/Method';
 import rateLimit from 'express-rate-limit';
 
 
 @Router.AuthOnly()
 export class UserController
-    extends Router.Controller
+    extends CrudController<User>
 {
     
-    @Inject({ ctorArgs: [ UserController.name ] })
-    protected _logger : Logger;
-    
-    @Inject()
-    protected _entityManagerWrapper : EntityManagerWrapper;
+    protected static readonly ENTITY = User;
     
     
     @Router.Endpoint.GET('/user/me', {
@@ -30,15 +25,31 @@ export class UserController
             authData : any
     ) : Promise<User>
     {
+        return this.getItem(authData.userId, [ 'stakePoolObservations' ]);
+    }
+    
+    
+    @Router.Endpoint.POST('/user/me', {
+        middlewares: [
+            rateLimit({ windowMs: 15 * 1000, max: 1 })
+        ]
+    })
+    @API.Endpoint(() => User)
+    public async postUserMe (
+        @Router.AuthData()
+            authData : any,
+        @Router.Body()
+        @Assert()
+            user : User
+    ) : Promise<User>
+    {
         const entityManager = this._entityManagerWrapper.getDirectEntityManager();
         const userRepository = entityManager.getRepository(User);
         
-        return await userRepository.findOne(
-            { id: authData.userId },
-            {
-                populate: [ 'stakePoolObservations' ]
-            }
-        );
+        
+        console.log(user);
+        
+        return user;
     }
     
 }
