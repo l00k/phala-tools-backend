e10a199ce03ce672e97ccad97167afe2c81e76ba
l00k <l00k+github@protonmail.com>
2022-04-08 16:37:19 +0200

    implementation batch

--- src/modules/Watchdog/Controller/UserController.ts
+++ src/modules/Watchdog/Controller/UserController.ts
@@ -26,7 +26,7 @@ export class UserController
             },
             account: '*',
             mode: true,
-            config: '*',
+            config: '**',
             lastNotifications: '*',
         }
     }, () => User)
