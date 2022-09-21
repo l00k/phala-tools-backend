64dd103d72b3d76863617c961cf49e5675d2d562
l00k <l00k+github@protonmail.com>
2022-09-21 19:32:07 +0200

    implementation batch

--- src/modules/App/CliApp.ts
+++ src/modules/App/CliApp.ts
@@ -1,19 +1,7 @@
 import { BaseCliApp } from '#/BackendCore/Module/BaseCliApp';
-import * as CLI from 'classy-commander';
 
 
 export class CliApp
     extends BaseCliApp
 {
-    
-    public async run () : Promise<void>
-    {
-        this.loadModules([
-            'Command'
-        ]);
-        
-        const argv = process.argv.splice(1);
-        await CLI.execute(argv);
-    }
-    
 }
